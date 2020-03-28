78721bce430086eb7466c8261e25ed6c47444245
Prashant Thapa <44457378+prashant523580@users.noreply.github.com>
2020-03-29 01:49:20 +0545

    Updated auto Carousel and text animation

--- navbar/src/js/main.js
+++ navbar/src/js/main.js
@@ -4,11 +4,6 @@ function scroll() {
     var navlink = document.getElementById("navlink");
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("navbar").style.padding = "5px 0px";
-        // if (navlink.className === "nav-link") {
-        //     navlink.className += " response";
-        // } else {
-        //     navlink.className = "navlink";
-        // }
         navlink.style.top = " 64px";
     } else {
         document.getElementById("navbar").style.padding = "20px 0px";
@@ -18,4 +13,18 @@ function scroll() {
 
 function changeFun(x) {
     x.classList.toggle("change");
+}
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000);
 }
